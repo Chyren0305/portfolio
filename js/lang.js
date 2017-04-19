80bfced2d4ced687493daf7e82b39491c80a3468
chyren0305 <chyren0305@google.com>
2017-04-19 18:00:40 +0800

    ~_~ add File

--- js/lang.js
+++ js/lang.js
@@ -4,11 +4,15 @@ var resources = {
             "title": "作品",
             "name": "",
             "job": "流水下山非有意，片雲歸洞本無心；<br/>人生若得如雲水，鐵樹開花遍界春。",
-            "ProjectsTitle": { "title": "作品", "online": "線上作業", "udacity": "線上課程作品","github":"GitHub" ,"demo":"一些作品"},
+            "menu": ["關於我", "作品集", "題庫本", "微專案", "聯絡我"],
+            "ProjectsTitle": { "title": "作品", "online": "題庫本", "udacity": "微專案", "github": "GitHub", "demo": "一些作品" },
             "option": {
                 "open": "開啟",
                 "close": "關閉"
             },
+
+
+            //作品
             "projects": [{
                 "title": "資策會個人作品",
                 "href": "山貓美式餐廳",
@@ -26,46 +30,54 @@ var resources = {
                 "href": "我個人的GitHub",
                 "about": "關於Git與GitHub",
                 "content": "進資策會前，曾到過github的網站，那時並不清楚是做什麼用的，直到資策會的友人告訴我後，才漸漸認真學這個。之後專案工作，也有用到這個，算是惠我良多。"
-            },{
-                "title": "Codecademy",
-                "href": "我的codecademy學習歷程",
-                "about": "關於Codecademy",
-                "content": "進資策會前，我到了這個網站，這也開啟我的前端旅程，這個如遊戲般又較簡單易上手的學習網站，讓人上癮，之後才決定進入資策會與其他相關學習資源徹底學習！"                
-            },{
-                "title": "FreeCodeCamp",
-                "href": "我的FreeCodeCamp學習歷程",
-                "about": "關於FreeCodeCamp",
-                "content": "2015年冬季時節，上了某社群網站看到有人分享了這個網站，於是乎，決定從2016年就拿來這個當每個星期的作業練習！"                
-            },{
-                "title": "線上互動履歷",
-                "href": "互動履歷",
-                "about": "關於互動履歷",
-                "content": "這個互動履歷目前只有英文版，它是由udacity課程Javascript Basics裡的作業教學，所完成的一個專案。"                
-            },{
-                "title": "青蛙過河小遊戲",
+            }, {
+                "title": "投票網站",
                 "href": "線上演示",
-                "about": "關於小遊戲",
-                "content": "這個HTML5的青蛙過小遊戲是由udacity課程object oriented javascript裡教學做出來的，在這裡我學會了canvas與一些簡單的設計模式。此遊戲只能在電腦上玩。按鍵盤上下鍵。"                
-            },{
-                "title": "響應式網頁",
-                "href": "線上演示",
-                "about": "關於響應式網頁",
-                "content": "這個響應式部落格網頁是由udacity課程Response Images與RWD兩個課程介紹，主要是RWD的作業，在這裡我學會使用grunt以及如何將圖片更有效利用與最佳化。不過主要只有圖片最佳化。"                
-            },{
+                "about": "關於這個網站",
+                "content": ""
+            }, {
                 "title": "樂心牙醫",
                 "href": "線上演示",
                 "about": "關於這個網站",
-                "content": ""                
-            },{
+                "content": ""
+            }, {
+                "title": "運動日誌",
+                "href": "線上演示",
+                "about": "關於響應式網頁",
+                "content": ""
+            }],
+
+            "QuizBanks": [{
+                "title": "Codecademy",
+                "href": "我的codecademy學習歷程",
+                "about": "關於Codecademy",
+                "content": "進資策會前，我到了這個網站，這也開啟我的前端旅程，這個如遊戲般又較簡單易上手的學習網站，讓人上癮，之後才決定進入資策會與其他相關學習資源徹底學習！"
+            }, {
+                "title": "FreeCodeCamp",
+                "href": "我的FreeCodeCamp學習歷程",
+                "about": "關於FreeCodeCamp",
+                "content": "2015年冬季時節，上了某社群網站看到有人分享了這個網站，於是乎，決定從2016年就拿來這個當每個星期的作業練習！"
+            }],
+            "otherDemos": [{
+                "title": "線上互動履歷",
+                "href": "互動履歷",
+                "about": "關於互動履歷",
+                "content": "這個互動履歷目前只有英文版，它是由udacity課程Javascript Basics裡的作業教學，所完成的一個專案。"
+            }, {
+                "title": "青蛙過河小遊戲",
+                "href": "線上演示",
+                "about": "關於小遊戲",
+                "content": "這個HTML5的青蛙過小遊戲是由udacity課程object oriented javascript裡教學做出來的，在這裡我學會了canvas與一些簡單的設計模式。此遊戲只能在電腦上玩。按鍵盤上下鍵。"
+            }, {
+                "title": "響應式網頁",
+                "href": "線上演示",
+                "about": "關於響應式網頁",
+                "content": "這個響應式部落格網頁是由udacity課程Response Images與RWD兩個課程介紹，主要是RWD的作業，在這裡我學會使用grunt以及如何將圖片更有效利用與最佳化。不過主要只有圖片最佳化。"
+            }, {
                 "title": "自己做的一些demo",
                 "href": "線上演示",
                 "about": "關於這些demo",
-                "content": ""                
-            },{
-                "title": "足球筆記",
-                "href": "線上演示",
-                "about": "關於響應式網頁",
-                "content": ""                
+                "content": ""
             }],
             "valueTrans": "值"
         }
@@ -75,7 +87,8 @@ var resources = {
             "title": "My Portfolio",
             "name": "Chyren Huang",
             "job": "Front-end Ninja",
-            "ProjectsTitle": { "title": "portfolio", "online": "OnlineCourse Homework","udacity": "OnlineCourse Projects" },
+            "menu": ["About", "Project", "Quiz", "Demo", "Contact"],
+            "ProjectsTitle": { "title": "portfolio", "online": "OnlineCourse Homework", "udacity": "OnlineCourse Projects" },
             "option": {
                 "open": "open",
                 "close": "close"
@@ -100,27 +113,27 @@ var resources = {
                 "title": "Future Codecademy",
                 "href": "link to my codecademy",
                 "about": "My Codecademy Personal Info",
-                "content": "In this online course I learned a lot and decided to be an great Front-end developer"                
-            },{
+                "content": "In this online course I learned a lot and decided to be an great Front-end developer"
+            }, {
                 "title": "FreeCodeCamp",
                 "href": "link to my FreeCodeCamp",
                 "about": "My FreeCodeCamp Homework Solution",
-                "content": "In this online course I learned a lot and decided to be an great Front-end developer"                
-            },{
+                "content": "In this online course I learned a lot and decided to be an great Front-end developer"
+            }, {
                 "title": "Resume",
                 "href": "Interational Resume",
                 "about": "My Interational Resume",
-                "content": "In this online course I learned many JQuery function and used it"                
-            },{
+                "content": "In this online course I learned many JQuery function and used it"
+            }, {
                 "title": "udacity Frogger Game",
                 "href": "Demo",
                 "about": "My First HTML5 Game",
-                "content": "In this online course I learned oriented object Javascript and some design patterns"                
-            },{
+                "content": "In this online course I learned oriented object Javascript and some design patterns"
+            }, {
                 "title": "RWD",
                 "href": "Demo",
                 "about": "this is my FreeCodeCamp homework solution",
-                "content": "In this online course I learned Response Website design basic concepts"                
+                "content": "In this online course I learned Response Website design basic concepts"
             }],
             "valueTrans": "value"
         }
@@ -128,20 +141,99 @@ var resources = {
 };
 
 $(document).ready(function() {
-    i18n.init({
-        "lng": 'zh_TW',
-        "resStore": resources,
-        "fallbackLng": 'zh_TW'
-    }, function(t) {
-        $(document).i18n();
-    });
+
+
+
+
+
+
+    //初始化 語言
+    languageInit()
+
+
+
+   
 
     $('.lang').click(function() {
         var lang = $(this).attr('data-lang');
         i18n.init({
             lng: lang
         }, function(t) {
-            $(document).i18n();
+            $(document).i18n()
         });
     });
+
+     DemoTemplate(languageInit('zh_TW'))
+
+
+     
+
+
 });
+
+
+//各個功能
+
+
+
+function languageInit(lang) {
+    i18n.init({
+        load: 'current', 
+        lng: lang,
+        resStore: resources,
+        fallbackLng: lang,
+        // debug:true
+    }, function(t) {
+
+        $(document).i18n()
+
+    })
+}
+
+
+function DemoTemplate() {
+    //Demo
+
+    $.getJSON("contact.json", function(data) {
+
+
+        var infoHtml = '';
+        $.each(data, function(template, detail) {
+
+            infoHtml += '<div class="col-sm-4 blur"> ';
+            // h4
+            infoHtml += '<h4><a href="' + detail.href + '"';
+            infoHtml += 'data-i18n="projects.' + template + '.href" target="_blank"></a></h4>';
+            //img
+            infoHtml += '<img src="' + detail.mainImg + '" class="img-responsive href" data-toggle="modal" data-target="#project' + template + '">';
+            // h4
+            infoHtml += '<h4 data-i18n="projects.' + template + '.title"></h4></div>';
+
+            //modal    
+            infoHtml += '<div class="modal fade" id="project' + template + '" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
+
+            //dialog
+            infoHtml += '<div class="modal-dialog"><div class="modal-content"><div class="modal-header">';
+            // h4
+            infoHtml += '<h4 class="modal-title" id="myModalLabel" data-i18n="projects.' + template + '.about"></h4></div>';
+
+            //body
+            infoHtml += '<div class="modal-body"><a href="' + detail.href + '" target="_blank">';
+            infoHtml += '<img class="img-responsive" src="' + detail.img + '"></a><p data-i18n="projects.' + template + '.content"></p></div>';
+
+            //footer            
+            infoHtml += '<div class="modal-footer">' +
+                '<button type="button" class="btn btn-default" data-dismiss="modal" data-i18n="option.close">Close</button>' + '</div></div></div></div>';
+
+        });
+
+        // modal
+
+
+        $('#allProject').html(infoHtml);
+
+        $("#allProject").i18n();
+    });
+
+
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+
+
     // gotop
     $(".gotop").click(function() {
         $("html,body").animate({
@@ -19,5 +22,115 @@ $(document).ready(function() {
 
 
 
+    //menu
 
+    //hover effect    
+    $('.menu__item').mouseover(function() {
+            $(this).addClass('menu__item--current');
+        })
+        .mouseout(function() {
+            $(this).removeClass('menu__item--current');
+        });
+
+
+    //錨點
+    $(".container").click(function() {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 1200);
+    });
+
+
+    $('.container').on('click', '.anchor', function(event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1200);
+
+        return false;
+    });
+
+
+
+    //判斷高度
+
+    var aboutMeTop = $('.aboutMe').offset().top;
+    var bannerTop = $('.banner').offset().top;
+    var projectsTop = $('#projects').offset().top;
+    var QuizBanksTop = $('#QuizBanks').offset().top;
+    var otherDemosTop = $('#otherDemos').offset().top;
+
+    $(window).scroll(function() {
+
+
+        //修正 使用狀態模式
+        if ($(window).scrollTop() >= otherDemosTop) {
+            $('.menu__item.contact').addClass('menu__item--current');
+
+            $('.menu__item.aboutMe').removeClass('menu__item--current');
+            $('.menu__item.Projects').removeClass('menu__item--current');
+            $('.menu__item.Quiz').removeClass('menu__item--current');
+            $('.menu__item.Demos').removeClass('menu__item--current');
+
+        } else if ($(window).scrollTop() >= QuizBanksTop) {
+            $('.menu__item.Demos').addClass('menu__item--current');
+
+            $('.menu__item.aboutMe').removeClass('menu__item--current');
+            $('.menu__item.Projects').removeClass('menu__item--current');
+            $('.menu__item.Quiz').removeClass('menu__item--current');
+            $('.menu__item.contact').removeClass('menu__item--current');
+
+        } else if ($(window).scrollTop() >= projectsTop) {
+            $('.menu__item.Quiz').addClass('menu__item--current');
+
+
+            $('.menu__item.aboutMe').removeClass('menu__item--current');
+            $('.menu__item.Projects').removeClass('menu__item--current');
+
+            $('.menu__item.Demos').removeClass('menu__item--current');
+            $('.menu__item.contact').removeClass('menu__item--current');
+
+        } else if ($(window).scrollTop() >= bannerTop) {
+
+            $('.menu__item.Projects').addClass('menu__item--current');
+
+            $('.menu__item.aboutMe').removeClass('menu__item--current');
+
+            $('.menu__item.Quiz').removeClass('menu__item--current');
+            $('.menu__item.Demos').removeClass('menu__item--current');
+            $('.menu__item.contact').removeClass('menu__item--current');
+
+        } else if ($(window).scrollTop() > aboutMeTop) {
+
+            $('.menu__list').css({ 'display': 'block', 'position': 'fixed' });
+            $('.menu__item.aboutMe').addClass('menu__item--current');
+
+            $('.menu__item.Projects').removeClass('menu__item--current');
+            $('.menu__item.Quiz').removeClass('menu__item--current');
+            $('.menu__item.Demos').removeClass('menu__item--current');
+            $('.menu__item.contact').removeClass('menu__item--current');
+
+
+        } else {
+            $('.menu__list').css({ 'display': 'flex', 'position': 'relative' });
+            $('.menu__item.aboutMe').removeClass('menu__item--current');
+            $('.menu__item.Projects').removeClass('menu__item--current');
+            $('.menu__item.Quiz').removeClass('menu__item--current');
+            $('.menu__item.Demos').removeClass('menu__item--current');
+            $('.menu__item.contact').removeClass('menu__item--current');
+
+        }
+
+
+
+        //較小畫面時的menu
+
+        $('.phone-bar').click(function() {
+            $('.menu__list-s').slideToggle();
+
+        });
+
+
+
+    });
 });
